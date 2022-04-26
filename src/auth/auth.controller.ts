06e8364b971bdb8326f662e853a3998ae8c1a120
Emmanuel Ashitey <eaashitey39@gmail.com>
2022-04-26 09:47:21 +0000

    Instantiate Auth Service in Auth Controller to be used

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,8 @@
 import { Controller } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller()
-export class AuthController {}
+export class AuthController {
+  // private authService: AuthService - creates an instance of the AuthService class
+  constructor(private authService: AuthService) {}
+}
