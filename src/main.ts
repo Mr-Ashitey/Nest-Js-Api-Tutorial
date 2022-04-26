2fed131edbc7b8fc9dca66571106a79c59f4a29c
Emmanuel Ashitey <eaashitey39@gmail.com>
2022-04-26 16:48:53 +0000

    Add global pipes and use Validation pipes to validate requests from endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // to remove any body inputs from the request which is not specified in the dto
+    }),
+  ); // to handle body validations from request
   await app.listen(3000);
 }
 bootstrap();
