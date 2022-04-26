de1b7055af319dd80e737610a560b4db7436afaa
Emmanuel Ashitey <eaashitey39@gmail.com>
2022-04-26 16:53:24 +0000

    Hash password from dto, create and store user data, remove password from return view and return the user.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {} //inject prisma service
   // sign in method/service
-  signIn() {
+  signIn(dto: AuthDto) {
     return { msg: 'I am signed in' };
   }
 
   // sign up method/service
-  signUp() {
-    return { msg: 'I am signed up' };
+  async signUp(dto: AuthDto) {
+    try {
+      // generate the password hash
+      const hash = await argon.hash(dto.password);
+
+      // save the new user in the db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          password: hash,
+        },
+      });
+
+      delete user.password; // remove the password hash from the response object
+      // return the saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        // throw a custom error if the email is already in use
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials already taken');
+        }
+      }
+      throw error;
+    }
   }
 }
