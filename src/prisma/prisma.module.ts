ff2602881a39b32ba35438e4c43880e76601d11d
Emmanuel Ashitey <eaashitey39@gmail.com>
2022-04-26 16:49:19 +0000

    Add prisma module and service

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,7 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
+@Global() // <-- this decorator is required to inject the service into other modules
 @Module({
   providers: [PrismaService],
+  exports: [PrismaService], // export the service so that it can be used by other modules
 })
 export class PrismaModule {}
