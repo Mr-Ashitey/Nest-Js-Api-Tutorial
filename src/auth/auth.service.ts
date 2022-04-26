9d82dd459428761755771598c30dfca259b02174
Emmanuel Ashitey <eaashitey39@gmail.com>
2022-04-26 10:01:43 +0000

    Added comments

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,9 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
+  // sign in method/service
   signIn() {
     return { msg: 'I am signed in' };
   }
+
+  // sign up method/service
   signUp() {
     return { msg: 'I am signed up' };
   }
