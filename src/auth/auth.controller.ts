e2936a337b631d623353d3ba5c049cdd771096cf
Emmanuel Ashitey <eaashitey39@gmail.com>
2022-04-26 09:55:53 +0000

    Created to routes, signin and signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
-@Controller()
+@Controller('auth') // to set the route
 export class AuthController {
   // private authService: AuthService - creates an instance of the AuthService class
   constructor(private authService: AuthService) {}
+  @Post('signin')
+  signIn() {
+    return 'I am signed in';
+  }
+
+  @Post('signup')
+  signUp() {
+    return 'I am signed up';
+  }
 }
